2e4305249ab4de79f5321324eb92591bc1ee5f8e
wnflq <wnflq@192.168.190.1>
2022-11-11 22:16:21 +0900

    test

--- team_I/src/team_I/MainFrame.java
+++ team_I/src/team_I/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 	public MainFrame() {
 		this.setSize(800, 600);
-		this.setTitle("team_I");
+		this.setTitle("test");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		this.setVisible(true);
