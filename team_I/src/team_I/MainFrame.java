8c552a061f9fa48ac925e3a0f7601981bfb80a8f
wnflq <wnflq@192.168.190.1>
2022-11-11 22:25:32 +0900

    add MainFrame

--- team_I/src/team_I/MainFrame.java
+++ team_I/src/team_I/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 	public MainFrame() {
 		this.setSize(800, 600);
-		this.setTitle("test");
+		this.setTitle("frame");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		this.setVisible(true);
